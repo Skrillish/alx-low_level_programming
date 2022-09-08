96c0e05cb7af87ad7fee8ae645d23ec2e993a70a
Skrillish <oluwasegunsamson444@gamil.com>
2022-09-08 15:28:43 -0700

    5-print.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
