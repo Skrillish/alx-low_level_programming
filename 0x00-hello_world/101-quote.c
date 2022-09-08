20aea3ee25a052ddac4adafdc834c1b7ad20ee85
Skrillish <oluwasegunsamson444@gamil.com>
2022-09-08 16:58:32 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include <unistd.h>
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ * main - Entry
+ *
+ * Return:1
  */
 int main(void)
 {
-        write(STDOUT_FILENO, "and that piece of art is useful
-                        \" - Dora Korpar, 2015-10-19\n", 59);
-         return (1);
+        char str1II = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	  
+	write(2, str1, 59)
+        return (1);
 }
