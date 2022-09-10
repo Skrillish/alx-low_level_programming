c9b74e002ab391c93dafd7b27fd78b247f1e78ff
Skrillish <oluwasegunsamson444@gamil.com>
2022-09-10 09:18:22 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, m);
 }
 else if (m < 6 && m != 0)
 {
-printf("Last digit of %d is %d and is less than 6 ans not 0\n", n, m);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 }
 else
 {
