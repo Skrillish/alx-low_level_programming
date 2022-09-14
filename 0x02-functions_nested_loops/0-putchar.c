1e9353e87cb9ad92470d9b27f9b1732db94489cd
Skrillish <oluwasegunsamson444@gamil.com>
2022-09-14 10:32:58 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entrypoint
+ *
+ * Description: Print _putchar
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+char name[10] = "_putchar";
+
+int i;
+
+for (i = 0; i < 8; i++)
+{
+_putchar(name[i]);
+}
+_putchar(10);
+return (0);
+}
