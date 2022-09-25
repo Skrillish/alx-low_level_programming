dd6d2144adf9b74810be5752fb7c769f16e0b476
Skrillish <oluwasegunsamson444@gamil.com>
2022-09-25 12:27:01 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints all digits
+ * Return: none
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+_putchar(i);
+_putchar('\n');
+}
