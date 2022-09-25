6118d4b5ee27a4014f7470fcdaf4a05a96320d52
Skrillish <oluwasegunsamson444@gamil.com>
2022-09-25 02:29:16 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of an address using its pointer variable
+ * @n: a pointer variable
+ * Return: none
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
