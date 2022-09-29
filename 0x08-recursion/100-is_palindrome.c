af2312adf28759f0ca39acf73a91e107f8ec2d75
Skrillish <oluwasegunsamson444@gamil.com>
2022-09-28 21:11:18 -0700

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * is_palindrome - checks if a string is a palindrome using recursion
+ * @s: string to be checked
+ * Return: 1 if it is a palindrome and 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+
+}
