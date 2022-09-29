c86d5b12312c42a5ce0343feb85205398d6b0d12
Skrillish <oluwasegunsamson444@gamil.com>
2022-09-28 20:49:10 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string using recursion
+ * @s: string input
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+if (*s != '\0')
+return (1 + _strlen_recursion(s + 1));
+return (0);
+}
