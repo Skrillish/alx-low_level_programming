3327a37da9fbcb4570084904f84bd1e13bedf358
Skrillish <oluwasegunsamson444@gamil.com>
2022-09-29 20:35:14 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,57 +1,47 @@
 #include "main.h"
 
-int check_palindrome(char *s);
-
 /**
- * is_palindrome - Returns if a string is palindrome
- * @s: the string value to be checked
+ * palind2 - obtains length of a
+ * @a: string
+ * @l: integer to count length
  *
- * Return: integer value
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int palind2(char *a, int l)
+{
+if (*a == 0)
+return (l - 1);
+return (palind2(a + 1, l + 1));
+}
+/**
+ * palind3 - compares string vs string reverse
+ * @a: string
+ * @l: length
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int palind3(char *a, int l)
+{
+if (*a != *(a + l))
+return (0);
+else if (*a == 0)
+return (1);
+return (palind3(a + 1, l - 2));
+}
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to evaluate
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int is_palindrome(char *s)
 {
-if (*s == '0')
-return (1);
+int l;
 
-return (check_palindrome(s));
-}
-
-/**
- * check_palindrome - Check if a string is palindrome
- * @s: the string value to be checked
- *
- * Return: integer value
- */
-int check_palindrome(char *s)
-{
-int l = _strlen_recursion(s) - 1;
-
-if (*s == s[l])
-{
-s++;
-l--;
-}
-else
-{
-return (0);
-}
-
-return (1);
-}
-
-/**
- * _strlen_recursion - Get the length of a string
- * @s: the string to get the length
- *
- * Return: the string length
- */
-int _strlen_recursion(char *s)
-{
-if (*s == '\0')
-{
-return (0);
-}
-
-s++;
-return (_strlen_recursion(s) + 1);
+l = palind2(s, 0);
+return (palind3(s, l));
 }
