0e06348cf1d10f79af4669171a656239664ebc37
Skrillish <oluwasegunsamson444@gamil.com>
2022-10-01 19:49:15 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- * _memset - Fill memory with a constant byte
- * @s: memory area to fill
- * @b: constant byte to fill
- * @n: bytes of memory area to fill
- *
- * Return: the memory area filled
+ * _memset - fills the first "n" characters of s with "b"
+ * @s: pointer to the string
+ * @b: character to replace with
+ * @n: number of characters to replace
+ * Return: address of s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int a;
-
-for (a = 0; a < n; a++)
+unsigned int i; /* has to be same type as n since you are comparing */
+for (i = 0; i < n; i++)
 {
-s[a] = b;
+s[i] = b;
 }
 return (s);
 }
