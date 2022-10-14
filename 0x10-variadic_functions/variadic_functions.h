09e0d93ded5c0d5472c0f3a002c29a53875ea5dc
Skrillish <oluwasegunsamson444@gamil.com>
2022-10-13 20:01:19 -0700

    variadic_functions.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,14 @@
+#ifndef _VARIADIC_FUNCTION_H_
+#define _VARIADIC_FUNCTION_H_
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int _putchar(char);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+
+#endif /* _VARIADIC_FUNCTION_H_ */
