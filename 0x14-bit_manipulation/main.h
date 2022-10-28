f44de8245d9f37bcc3427ec273451a3a673e1844
Skrillish <oluwasegunsamson444@gamil.com>
2022-10-27 20:18:05 -0700

    main.h

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int _putchar(char);
+unsigned int npower(unsigned int, unsigned int);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+#endif /* MAIN_H */
